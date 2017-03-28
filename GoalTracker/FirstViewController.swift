7e0d9f5a1d03c76b32e16227220de0d05f4f1be7
IZ <iz@Isaacs-MacBook-Air.local>
2017-03-28 10:25:57 -0400

    Initial Commit

---
+++ GoalTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GoalTracker
+//
+//  Created by Isaac Zhu on 3/28/17.
+//  Copyright © 2017 Isaac Zhu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
